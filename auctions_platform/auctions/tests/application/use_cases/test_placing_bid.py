27c97190f9fb65756525a3f5c68a3a01716c2197
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-08-27 12:56:51 +0200

    feat: add foundation lib

--- auctions_platform/auctions/tests/application/use_cases/test_placing_bid.py
+++ auctions_platform/auctions/tests/application/use_cases/test_placing_bid.py
@@ -1,5 +1,40 @@
+from unittest.mock import Mock
+
+import pytest
+
+from foundation.value_objects import Money
+from foundation.value_objects.factories import get_dollars
+
+from auctions.application.use_cases import PlacingBid
+from auctions.application.use_cases.placing_bid import (
+    PlacingBidInputDto,
+    PlacingBidOutputDto,
+)
+from auctions.domain.entities import Auction
+
+
+@pytest.fixture()
+def bidder_id() -> int:
+    return 1
+
+
+@pytest.fixture()
+def amount() -> Money:
+    return get_dollars("12.00")
+
+
+@pytest.fixture()
+def input_dto(
+    auction: Auction, bidder_id: int, amount: Money
+) -> PlacingBidInputDto:
+    return PlacingBidInputDto(bidder_id, auction.id, amount)
+
+
 def test_presents_output_dto(
-    placing_bid_uc: PlacingBid, input_dto: PlacingBidInputDto, placing_bid_output_boundary_mock: Mock, auction: Auction
+    placing_bid_uc: PlacingBid,
+    input_dto: PlacingBidInputDto,
+    placing_bid_output_boundary_mock: Mock,
+    auction: Auction
 ) -> None:
     placing_bid_uc.execute(input_dto)
 
